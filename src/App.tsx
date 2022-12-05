564fca2fa6ca3785f273827114747311de86294f
Filip <ffilip.djokic@gmail.com>
2022-12-05 23:57:24 +0100

    adding react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import { TableComponent } from "./components/Table/TableComponent";
 import { Loading } from "./components/Loading/Loading";
+import { Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <TableComponent />
       <Loading />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TableComponent />} />
+          <Route path="/add-book" element={<div>add</div>} />
+        </Routes>
+      </Router>
     </>
   );
 }
